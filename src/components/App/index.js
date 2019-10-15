c6af6e3622dd82759c61dd474040a9c20c896b28
Tim Haley <tim.haley@getbellhops.com>
2019-10-14 22:06:22 -0400

    Add github ribbon

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ribbon from "react-github-ribbon";
 import { MapProvider } from '../../logic';
 import { About, Map, Footer } from '..';
 import styles from './index.module.css';
@@ -7,6 +8,12 @@ function App() {
   return (
     <MapProvider>
       <div className={ styles.container }>
+        <Ribbon
+          user="timhaley94"
+          repo="markov_maps"
+          fill="2d2d2d"
+          color="white"
+        />
         <About />
         <Map />
         <Footer />
